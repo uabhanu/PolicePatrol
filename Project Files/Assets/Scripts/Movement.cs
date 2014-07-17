14a822270318e53ad1800f43d538900e7bffd8c2
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-07-17 18:21:07 +0100

    17th July 2014 @ 18:20 Level 1 Build WIP

---
+++ Project Files/Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour 
+{
+	public float speed;
+	
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		transform.Translate(speed , 0 , 0);	
+	}
+}
