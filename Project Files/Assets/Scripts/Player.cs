bf500e6b997cef9d44f2088dc0799f3cdd50465d
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-07-17 20:26:10 +0100

    17th July 2014 @ 20:25 Player Select/Deselect WIP

---
+++ Project Files/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public bool selected;
+	public float speed;
+	
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.tag.Equals("Enemy"))
+		{
+			Debug.Log("Collided");
+		}
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log("Player Selected");
+		selected = true;
+	}
+}
