a1b055831d085a29f4a148773e593e7c060aa066
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-07-18 17:55:31 +0100

    18th July 2014 @ 17:55 WayPoints good to go

--- Project Files/Assets/Scripts/WayPoint.cs
+++ Project Files/Assets/Scripts/WayPoint.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class WayPoint : MonoBehaviour 
 {
+	private int breakWhile = 1;
 	public Collider[] wayPointColliders;
+	public int wayPointDistance = 10;
+	public LayerMask layer = 1 << 8;
 	
 	void Start () 
 	{
-		wayPointColliders = Physics.OverlapSphere(transform.position , 20);
+		while(wayPointColliders.Length < 3 && breakWhile < 10)
+		{
+			wayPointColliders = Physics.OverlapSphere(transform.position , wayPointDistance , layer);
+			wayPointDistance += 10;
+			breakWhile++;
+		}
 	}
 
 	void Update () 
