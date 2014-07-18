e2f12016d46c6b7d857817fcfa9ac5b09a8162af
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-07-18 16:42:54 +0100

    18th July 2014 @ 16:42 WayPoint.cs created & PF_WayPoints.prefab updated

---
+++ Project Files/Assets/Scripts/WayPoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WayPoint : MonoBehaviour 
+{
+	public Collider[] wayPointColliders;
+	
+	void Start () 
+	{
+		wayPointColliders = Physics.OverlapSphere(transform.position , 20);
+	}
+
+	void Update () 
+	{
+	
+	}
+}
