3e26ffef4820451a1267c63be2275881dd8e2778
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-07-19 09:30:22 +0100

    19th July 2014 @ 09:30 WayPointRadio created

--- Project Files/Assets/Scripts/WayPoint.cs
+++ Project Files/Assets/Scripts/WayPoint.cs
@@ -3,18 +3,39 @@ using System.Collections;
 
 public class WayPoint : MonoBehaviour 
 {
-	private int breakWhile = 1;
+	private int breakWhile = 1 , countDown = 3;
 	public Collider[] wayPointColliders;
 	public int wayPointDistance = 10;
 	public LayerMask layer = 1 << 8;
 	
-	void Start () 
+	void Awake () 
 	{
 		while(wayPointColliders.Length < 3 && breakWhile < 10)
 		{
 			wayPointColliders = Physics.OverlapSphere(transform.position , wayPointDistance , layer);
 			wayPointDistance += 10;
 			breakWhile++;
+		}
+	}
+	
+	public void SetData(int incomingData)
+	{
+		countDown = incomingData;
+		countDown--;
+
+		Debug.Log("WayPoint : " + this.name + " Sent the countdown of : " + countDown);
+
+		SendData();
+	}
+
+	public void SendData()
+	{
+		if(countDown > 0)
+		{
+			foreach(Collider wayPointCollider in wayPointColliders)
+			{
+				wayPointCollider.gameObject.GetComponent<WayPoint>().SetData(countDown);
+			}
 		}
 	}
 
---
+++ Project Files/Assets/Scripts/WayPointRadio.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WayPointRadio : MonoBehaviour 
+{
+	public WayPoint wayPointScript;
+	
+	void Start () 
+	{
+		if(wayPointScript != null)
+		{
+			wayPointScript.SetData(3);
+		}
+	}
+
+	void Update () 
+	{
+	
+	}
+}
