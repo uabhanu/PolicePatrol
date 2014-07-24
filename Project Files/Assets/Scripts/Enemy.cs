d184933745d57f300a1c4f4d30950c1b8ebd3b49
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-07-24 13:00:11 +0100

    24th July 2014 @ 13:00 Enemy AI back to square 1

--- Project Files/Assets/Scripts/Enemy.cs
+++ Project Files/Assets/Scripts/Enemy.cs
@@ -5,14 +5,6 @@ using UnityEngine;
 public class Enemy : MonoBehaviour 
 {
 	public Animator anim;
-	public float enemyToWayPointDistance , smoothTime;
-	public GameObject closestWayPoint , wayPointControlObj;
-	//public GameObject[] destinationObjs;
-	public List<Transform> destinations;
-	//public Transform truckLocation;
-	public List<Transform> truckLocation;
-	public Vector3 velocity;
-	public WayPointController wayPointControllerScript;
 
 	public enum State
 	{
@@ -33,22 +25,6 @@ public class Enemy : MonoBehaviour
 			anim = GetComponent<Animator>();
 		}
 
-		//destinationObjs = GameObject.FindGameObjectsWithTag("Destination");
-
-		wayPointControlObj = GameObject.FindGameObjectWithTag("WayPointControl");
-
-		if(wayPointControlObj != null)
-		{
-			wayPointControllerScript = wayPointControlObj.GetComponent<WayPointController>();
-		}
-
-		closestWayPoint = wayPointControllerScript.FindClosestWayPoint(transform);
-
-		if(closestWayPoint != null)
-		{
-			truckLocation = closestWayPoint.GetComponent<WayPoint>().GetTruckLocation();
-		}
-
 		if(transform.position.y < 0)
 		{
 			anim.SetInteger("AnimIndex" , 1);
@@ -57,7 +33,7 @@ public class Enemy : MonoBehaviour
 
 	void Movement()
 	{
-		transform.position = Vector3.SmoothDamp(transform.position , closestWayPoint.transform.position , ref velocity , smoothTime);
+		//transform.position = Vector3.SmoothDamp(transform.position , closestWayPoint.transform.position , ref velocity , smoothTime);
 	}
 
 	public void SetState(int newState)
--- Project Files/Assets/Scripts/WayPoint.cs
+++ Project Files/Assets/Scripts/WayPoint.cs
@@ -4,56 +4,14 @@ using UnityEngine;
 
 public class WayPoint : MonoBehaviour 
 {
-	private int breakWhile = 1;
-	public Collider[] wayPointColliders;
-	public int wayPointDistance = 10;
-	public LayerMask layer = 1 << 8;
-	public List<Transform> truckLocation;
-	
-	void Awake () 
+	void Start () 
 	{
-		truckLocation = new List<Transform>();
 
-		while(wayPointColliders.Length < 3 && breakWhile < 10)
-		{
-			wayPointColliders = Physics.OverlapSphere(transform.position , wayPointDistance , layer);
-			wayPointDistance += 10;
-			breakWhile++;
-		}
 	}
 
 	public void InitializeData()
 	{
-		truckLocation.Clear();
-		SetData(truckLocation);
-	}
-	
-	public void SetData(List<Transform> newList)
-	{
-		//Debug.Log("SetData Method");
 
-		if(truckLocation.Count == 0 || newList[0] != truckLocation[0] || newList.Count < truckLocation.Count)
-		{
-			truckLocation = newList;
-			truckLocation.Add (transform);
-			SendData();
-		}
-	}
-
-	public void SendData()
-	{
-		foreach(Collider wayPointCollider in wayPointColliders)
-		{
-			if(!truckLocation.Contains(wayPointCollider.transform))
-			{
-				wayPointCollider.gameObject.GetComponent<WayPoint>().SetData(new List<Transform>(truckLocation));
-			}
-		}
-	}
-
-	public List<Transform> GetTruckLocation()
-	{
-		return new List<Transform>(truckLocation);
 	}
 
 	void Update () 
