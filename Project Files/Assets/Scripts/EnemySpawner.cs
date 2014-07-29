66d682201e9861b5dd5cfc39e8ad03f790e1a476
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-07-29 15:21:55 +0100

    29th July 2014 @ 15:21 Enemies Path Refined

--- Project Files/Assets/Scripts/EnemySpawner.cs
+++ Project Files/Assets/Scripts/EnemySpawner.cs
@@ -21,11 +21,11 @@ public class EnemySpawner : MonoBehaviour
 
 		i = enemies;
 
-		randomLocations[0] = new Vector2(-1.7f , -4.2f);
+		randomLocations[0] = new Vector2(-2.3f , -4.2f);
 		randomLocations[1] = new Vector2(-4.2f , 4.2f);
 		randomLocations[2] = new Vector2(1.4f , 4.2f);
 		randomLocations[3] = new Vector2(1.7f , -4.2f);
-		randomLocations[4] = new Vector2(1.4f , 4.2f);
+		randomLocations[4] = new Vector2(-1.7f , 4.2f);
 		randomLocations[5] = new Vector2(-5.2f , -4.2f);
 		randomLocations[6] = new Vector2(4.2f , 4.2f);
 		randomLocations[7] = new Vector2(5.2f , -4.2f);
@@ -55,7 +55,7 @@ public class EnemySpawner : MonoBehaviour
 	IEnumerator SpawnLocationTimer()
 	{
 		yield return new WaitForSeconds(32);
-		i -= (maxEnemies - 1);
+		i = 0;
 		StartCoroutine("SpawnLocationTimer");
 	}
 
