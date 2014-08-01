a8eefd7ef572aa706712fdb0790eda4722a5eb03
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-08-01 16:24:59 +0100

    1st Aug 2014 @ 16:24 Basic Level Build with Navigation Done. MecAnim WIP

--- ProjectFiles(3D)/Assets/Scripts/EnemySpawner.cs
+++ ProjectFiles(3D)/Assets/Scripts/EnemySpawner.cs
@@ -12,14 +12,14 @@ public class EnemySpawner : MonoBehaviour
 	{	
 		i = enemies;
 		
-		randomLocations[0] = new Vector3(-32.0f , 2 , 37.0f);
-		randomLocations[1] = new Vector3(-11.0f , 2 , 37.0f);
-		randomLocations[2] = new Vector3(11.0f , 2 , 37.0f);
-		randomLocations[3] = new Vector3(31.0f , 2 , 37.0f);
-		randomLocations[4] = new Vector3(-38.0f , 2 , -24.0f);
-		randomLocations[5] = new Vector3(-13.0f , 2 , -24.0f);
-		randomLocations[6] = new Vector3(13.0f , 2 , -24.0f);
-		randomLocations[7] = new Vector3(38.0f , 2 , -24.0f);
+		randomLocations[0] = new Vector3(-34.0f , 2 , 39.0f);
+		randomLocations[1] = new Vector3(-14.0f , 2 , 39.0f);
+		randomLocations[2] = new Vector3(6.0f , 2 , 39.0f);
+		randomLocations[3] = new Vector3(27.0f , 2 , 39.0f);
+		randomLocations[4] = new Vector3(-34.0f , 2 , -36.0f);
+		randomLocations[5] = new Vector3(-14.0f , 2 , -36.0f);
+		randomLocations[6] = new Vector3(6.0f , 2 , -36.0f);
+		randomLocations[7] = new Vector3(26.0f , 2 , -36.0f);
 		
 		StartCoroutine("EnemySpawnTimer");
 		StartCoroutine("SpawnLocationTimer");
