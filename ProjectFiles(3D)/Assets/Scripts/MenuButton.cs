14327edec07f152717200e8ccc70a72a7cf37414
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-08-05 17:10:22 +0100

    5th Aug 2014 @ 17:10 Selection Screen created

--- ProjectFiles(3D)/Assets/Scripts/MenuButton.cs
+++ ProjectFiles(3D)/Assets/Scripts/MenuButton.cs
@@ -9,21 +9,116 @@ public class MenuButton : MonoBehaviour
 	
 	void Start () 
 	{
-		Time.timeScale = 1;
+
 	}
 
 	void ButtonClick(string buttonName)
 	{
 		switch(buttonName)
 		{
+			case "Level1" :
+				Application.LoadLevel("Level1");
+			break;
+
+			case "Level2" :
+				Application.LoadLevel("Level2");
+			break;
+
+			case "Level3" :
+				Application.LoadLevel("Level3");
+			break;
+
+			case "Level4" :
+				Application.LoadLevel("Level4");
+			break;
+
+			case "Level5" :
+				Application.LoadLevel("Level5");
+			break;
+
+			case "Level6" :
+				Application.LoadLevel("Level6");
+			break;
+
+			case "Level7" :
+				Application.LoadLevel("Level7");
+			break;
+
+			case "Level8" :
+				Application.LoadLevel("Level8");
+			break;
+
+			case "Level9" :
+				Application.LoadLevel("Level9");
+			break;
+
+			case "Level10" :
+				Application.LoadLevel("Level10");
+			break;
+
+			case "Level11" :
+				Application.LoadLevel("Level11");
+			break;
+
+			case "Level12" :
+				Application.LoadLevel("Level12");
+			break;
+
+			case "Level13" :
+				Application.LoadLevel("Level13");
+			break;
+
+			case "Level14" :
+				Application.LoadLevel("Level14");
+			break;
+
+			case "Level15" :
+				Application.LoadLevel("Level15");
+			break;
+
+			case "Level16" :
+				Application.LoadLevel("Level16");
+			break;
+
+			case "Level17" :
+				Application.LoadLevel("Level17");
+			break;
+
+			case "Level18" :
+				Application.LoadLevel("Level18");
+			break;
+
+			case "Level19" :
+				Application.LoadLevel("Level19");
+			break;
+
+			case "Level20" :
+				Application.LoadLevel("Level20");
+			break;
+
+			case "Level21" :
+				Application.LoadLevel("Level21");
+			break;
+
+			case "Level22" :
+				Application.LoadLevel("Level22");
+			break;
+
 			case "Pause" :
+				Debug.Log("PauseButton");
 				Time.timeScale = 0;
 				iguiScript.Inactive("PauseButton");
 				iguiScript.Active("QuitButton");
 				iguiScript.Active("ResumeButton");
 			break;
+
+			case "Quit" :
+				Debug.Log("QuitButton");
+				Application.Quit();
+			break;
 				
 			case "Resume" :
+				Debug.Log("ResumeButton");	
 				Time.timeScale = 1;
 				iguiScript.Active("PauseButton");
 				iguiScript.Inactive("QuitButton");
@@ -34,9 +129,124 @@ public class MenuButton : MonoBehaviour
 
 	void OnMouseDown()
 	{
+		if(buttonName == "Level1")
+		{
+			ButtonClick("Level1");
+		}
+
+		if(buttonName == "Level2")
+		{
+			ButtonClick("Level2");
+		}
+
+		if(buttonName == "Level3")
+		{
+			ButtonClick("Level3");
+		}
+
+		if(buttonName == "Level4")
+		{
+			ButtonClick("Level4");
+		}
+
+		if(buttonName == "Level5")
+		{
+			ButtonClick("Level5");
+		}
+
+		if(buttonName == "Level6")
+		{
+			ButtonClick("Level6");
+		}
+
+		if(buttonName == "Level7")
+		{
+			ButtonClick("Level7");
+		}
+
+		if(buttonName == "Level8")
+		{
+			ButtonClick("Level8");
+		}
+
+		if(buttonName == "Level9")
+		{
+			ButtonClick("Level9");
+		}
+
+		if(buttonName == "Level10")
+		{
+			ButtonClick("Level10");
+		}
+
+		if(buttonName == "Level11")
+		{
+			ButtonClick("Level11");
+		}
+
+		if(buttonName == "Level12")
+		{
+			ButtonClick("Level12");
+		}
+
+		if(buttonName == "Level13")
+		{
+			ButtonClick("Level13");
+		}
+
+		if(buttonName == "Level14")
+		{
+			ButtonClick("Level14");
+		}
+
+		if(buttonName == "Level15")
+		{
+			ButtonClick("Level15");
+		}
+
+		if(buttonName == "Level16")
+		{
+			ButtonClick("Level16");
+		}
+
+		if(buttonName == "Level17")
+		{
+			ButtonClick("Level17");
+		}
+
+		if(buttonName == "Level18")
+		{
+			ButtonClick("Level18");
+		}
+
+		if(buttonName == "Level19")
+		{
+			ButtonClick("Level19");
+		}
+
+		if(buttonName == "Level20")
+		{
+			ButtonClick("Level20");
+		}
+
+		if(buttonName == "Level21")
+		{
+			ButtonClick("Level21");
+		}
+
+		if(buttonName == "Level22")
+		{
+			ButtonClick("Level22");
+		}
+
 		if(buttonName == "Pause")
 		{
 			ButtonClick("Pause");
+		}
+
+		if(buttonName == "Quit")
+		{
+			ButtonClick("Quit");
 		}
 
 		if(buttonName == "Resume")
---
+++ ProjectFiles(3D)/Assets/Scripts/Selection.cs
@@ -0,0 +1,271 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Selection : MonoBehaviour 
+{
+	public BoxCollider2D[] buttonColliders;
+	public GUITexture[] buttonSprites;
+	
+	void Start () 
+	{
+		Unlock("Level1");
+		Lock("Level2");
+		Lock("Level3");
+		Lock("Level4");
+		Lock("Level5");
+		Lock("Level6");
+		Lock("Level7");
+		Lock("Level8");
+		Lock("Level9");
+		Lock("Level10");
+		Lock("Level11");
+		Lock("Level12");
+		Lock("Level13");
+		Lock("Level14");
+		Lock("Level15");
+		Lock("Level16");
+		Lock("Level17");
+		Lock("Level18");
+		Lock("Level19");
+		Lock("Level20");
+		Lock("Level21");
+		Lock("Level22");
+	}
+
+	public void Lock(string name)
+	{
+		switch(name)
+		{
+			case "Level1" :
+				buttonColliders[0].enabled = false;
+				buttonSprites[0].guiTexture.color = Color.red;
+			break;
+
+			case "Level2" :
+				buttonColliders[1].enabled = false;
+				buttonSprites[1].guiTexture.color = Color.red;
+			break;
+
+			case "Level3" :
+				buttonColliders[2].enabled = false;
+				buttonSprites[2].guiTexture.color = Color.red;
+			break;
+
+			case "Level4" :
+				buttonColliders[3].enabled = false;
+				buttonSprites[3].guiTexture.color = Color.red;
+			break;
+
+			case "Level5" :
+				buttonColliders[4].enabled = false;
+				buttonSprites[4].guiTexture.color = Color.red;
+			break;
+
+			case "Level6" :
+				buttonColliders[5].enabled = false;
+				buttonSprites[5].guiTexture.color = Color.red;
+			break;
+
+			case "Level7" :
+				buttonColliders[6].enabled = false;
+				buttonSprites[6].guiTexture.color = Color.red;
+			break;
+
+			case "Level8" :
+				buttonColliders[7].enabled = false;
+				buttonSprites[7].guiTexture.color = Color.red;
+			break;
+
+			case "Level9" :
+				buttonColliders[8].enabled = false;
+				buttonSprites[8].guiTexture.color = Color.red;
+			break;
+
+			case "Level10" :
+				buttonColliders[9].enabled = false;
+				buttonSprites[9].guiTexture.color = Color.red;
+			break;
+
+			case "Level11" :
+				buttonColliders[10].enabled = false;
+				buttonSprites[10].guiTexture.color = Color.red;
+			break;
+
+			case "Level12" :
+				buttonColliders[11].enabled = false;
+				buttonSprites[11].guiTexture.color = Color.red;
+			break;
+
+			case "Level13" :
+				buttonColliders[12].enabled = false;
+				buttonSprites[12].guiTexture.color = Color.red;
+			break;
+
+			case "Level14" :
+				buttonColliders[13].enabled = false;
+				buttonSprites[13].guiTexture.color = Color.red;
+			break;
+
+			case "Level15" :
+				buttonColliders[14].enabled = false;
+				buttonSprites[14].guiTexture.color = Color.red;
+			break;
+
+			case "Level16" :
+				buttonColliders[15].enabled = false;
+				buttonSprites[15].guiTexture.color = Color.red;
+			break;
+
+			case "Level17" :
+				buttonColliders[16].enabled = false;
+				buttonSprites[16].guiTexture.color = Color.red;
+			break;
+
+			case "Level18" :
+				buttonColliders[17].enabled = false;
+				buttonSprites[17].guiTexture.color = Color.red;
+			break;
+
+			case "Level19" :
+				buttonColliders[18].enabled = false;
+				buttonSprites[18].guiTexture.color = Color.red;
+			break;
+
+			case "Level20" :
+				buttonColliders[19].enabled = false;
+				buttonSprites[19].guiTexture.color = Color.red;
+			break;
+
+			case "Level21" :
+				buttonColliders[20].enabled = false;
+				buttonSprites[20].guiTexture.color = Color.red;
+			break;
+
+			case "Level22" :
+				buttonColliders[21].enabled = false;
+				buttonSprites[21].guiTexture.color = Color.red;
+			break;
+		}
+	}
+
+	public void Unlock(string name)
+	{
+		switch(name)
+		{
+			case "Level1" :
+				buttonColliders[0].enabled = true;
+				buttonSprites[0].guiTexture.color = Color.green;
+			break;
+			
+			case "Level2" :
+				buttonColliders[1].enabled = true;
+				buttonSprites[1].guiTexture.color = Color.green;
+			break;
+			
+			case "Level3" :
+				buttonColliders[2].enabled = true;
+				buttonSprites[2].guiTexture.color = Color.green;
+			break;
+			
+			case "Level4" :
+				buttonColliders[3].enabled = true;
+				buttonSprites[3].guiTexture.color = Color.green;
+			break;
+			
+			case "Level5" :
+				buttonColliders[4].enabled = true;
+				buttonSprites[4].guiTexture.color = Color.green;
+			break;
+			
+			case "Level6" :
+				buttonColliders[5].enabled = true;
+				buttonSprites[5].guiTexture.color = Color.green;
+			break;
+			
+			case "Level7" :
+				buttonColliders[6].enabled = true;
+				buttonSprites[6].guiTexture.color = Color.green;
+			break;
+			
+			case "Level8" :
+				buttonColliders[7].enabled = true;
+				buttonSprites[7].guiTexture.color = Color.green;
+			break;
+			
+			case "Level9" :
+				buttonColliders[8].enabled = true;
+				buttonSprites[8].guiTexture.color = Color.green;
+			break;
+			
+			case "Level10" :
+				buttonColliders[9].enabled = true;
+				buttonSprites[9].guiTexture.color = Color.green;
+			break;
+			
+			case "Level11" :
+				buttonColliders[10].enabled = true;
+				buttonSprites[10].guiTexture.color = Color.green;
+			break;
+			
+			case "Level12" :
+				buttonColliders[11].enabled = true;
+				buttonSprites[11].guiTexture.color = Color.green;
+			break;
+			
+			case "Level13" :
+				buttonColliders[12].enabled = true;
+				buttonSprites[12].guiTexture.color = Color.green;
+			break;
+			
+			case "Level14" :
+				buttonColliders[13].enabled = true;
+				buttonSprites[13].guiTexture.color = Color.green;
+			break;
+			
+			case "Level15" :
+				buttonColliders[14].enabled = true;
+				buttonSprites[14].guiTexture.color = Color.green;
+			break;
+			
+			case "Level16" :
+				buttonColliders[15].enabled = true;
+				buttonSprites[15].guiTexture.color = Color.green;
+			break;
+			
+			case "Level17" :
+				buttonColliders[16].enabled = true;
+				buttonSprites[16].guiTexture.color = Color.green;
+			break;
+			
+			case "Level18" :
+				buttonColliders[17].enabled = true;
+				buttonSprites[17].guiTexture.color = Color.green;
+			break;
+			
+			case "Level19" :
+				buttonColliders[18].enabled = true;
+				buttonSprites[18].guiTexture.color = Color.green;
+			break;
+			
+			case "Level20" :
+				buttonColliders[19].enabled = true;
+				buttonSprites[19].guiTexture.color = Color.green;
+			break;
+			
+			case "Level21" :
+				buttonColliders[20].enabled = true;
+				buttonSprites[20].guiTexture.color = Color.green;
+			break;
+			
+			case "Level22" :
+				buttonColliders[21].enabled = true;
+				buttonSprites[21].guiTexture.color = Color.green;
+			break;
+		}
+	}
+
+	void Update () 
+	{
+	
+	}	
+}
