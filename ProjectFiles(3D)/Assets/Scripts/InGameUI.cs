00306faa69210e8dccc91538cce9e92778ef3a09
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-08-05 13:39:48 +0100

    InGame Menu Finished

--- ProjectFiles(3D)/Assets/Scripts/InGameUI.cs
+++ ProjectFiles(3D)/Assets/Scripts/InGameUI.cs
@@ -3,12 +3,57 @@ using System.Collections;
 
 public class InGameUI : MonoBehaviour 
 {
+	public CircleCollider2D[] buttonColliders;
 	public GUIText truckLeftScoreLabel , truckRightScoreLabel;
+	public GUITexture[] buttonSprites;
 	public int truckLeftScoreValue , truckRightScoreValue;
 
 	void Start () 
 	{
-	
+		Inactive("QuitButton");
+		Inactive("ResumeButton");
+	}
+
+	public void Active(string name)
+	{
+		switch(name)
+		{
+			case "PauseButton" :
+				buttonSprites[0].gameObject.SetActive(true);
+				buttonColliders[0].gameObject.SetActive(true);
+			break;
+
+			case "QuitButton" :
+				buttonSprites[1].gameObject.SetActive(true);
+				buttonColliders[1].gameObject.SetActive(true);
+			break;
+
+			case "ResumeButton" :
+				buttonSprites[2].gameObject.SetActive(true);
+				buttonColliders[2].gameObject.SetActive(true);
+			break;
+		}
+	}
+
+	public void Inactive(string name)
+	{
+		switch(name)
+		{
+			case "PauseButton" :
+				buttonSprites[0].gameObject.SetActive(false);
+				buttonColliders[0].gameObject.SetActive(false);
+			break;
+
+			case "QuitButton" :
+				buttonSprites[1].gameObject.SetActive(false);
+				buttonColliders[1].gameObject.SetActive(false);
+			break;
+			
+			case "ResumeButton" :
+				buttonSprites[2].gameObject.SetActive(false);
+				buttonColliders[2].gameObject.SetActive(false);
+			break;
+		}
 	}
 
 	void Update () 
---
+++ ProjectFiles(3D)/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButton : MonoBehaviour 
+{
+	public InGameUI iguiScript;
+	public GameObject iguiObj;
+	public string buttonName , level , relativePath;
+	
+	void Start () 
+	{
+		Time.timeScale = 1;
+	}
+
+	void ButtonClick(string buttonName)
+	{
+		switch(buttonName)
+		{
+			case "Pause" :
+				Time.timeScale = 0;
+				iguiScript.Inactive("PauseButton");
+				iguiScript.Active("QuitButton");
+				iguiScript.Active("ResumeButton");
+			break;
+				
+			case "Resume" :
+				Time.timeScale = 1;
+				iguiScript.Active("PauseButton");
+				iguiScript.Inactive("QuitButton");
+				iguiScript.Inactive("ResumeButton");
+			break;
+		}
+	}
+
+	void OnMouseDown()
+	{
+		if(buttonName == "Pause")
+		{
+			ButtonClick("Pause");
+		}
+
+		if(buttonName == "Resume")
+		{
+			ButtonClick("Resume");
+		}
+	}
+
+	void Update () 
+	{
+	
+	}	
+}
