5fe6a270d5cea8a62b0a632747b42133acb9cd65
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-08-16 16:00:33 +0100

    GUIPosition script added PF_GameArea updated

---
+++ ProjectFiles(3D)/Assets/Scripts/GUIPosition.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIPosition : MonoBehaviour 
+{
+	private Rect currentRect;
+	
+	void Start () 
+	{
+		currentRect = new Rect(15 , 15 , 150 , 150);
+	}
+
+	void OnGUI()
+	{
+		//Debug.Log("OnGUI()");
+		currentRect.x = (Screen.width * 0.5f) - (currentRect.width * 0.5f);
+		currentRect.y = (Screen.height * 0.5f) - (currentRect.height * 0.5f);
+	}
+
+	void Update () 
+	{
+	
+	}
+}
