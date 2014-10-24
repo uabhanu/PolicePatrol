238a0d4a5b34b9bdf1f09a5ede06b8dc9b2ea0e6
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-10-24 09:40:42 +0100

    Enemy Spawner Improved. Testing to be done

--- ProjectFiles(3D)/Assets/Scripts/EnemySpawner.cs
+++ ProjectFiles(3D)/Assets/Scripts/EnemySpawner.cs
@@ -5,7 +5,7 @@ public class EnemySpawner : MonoBehaviour
 {
 	//[HideInInspector]
 	public InGameUI iguiScript;
-	public float spawnTimer , spawnLocationX , spawnLocationZ;
+	public float firstEnemyTime , spawnTimer , spawnLocationX , spawnLocationZ;
 	public float[] xPositions , zPositions;
 	public GameObject iguiObj , thugObj;
 	public int randomPos;
@@ -26,19 +26,31 @@ public class EnemySpawner : MonoBehaviour
 		}
 				
 		StartCoroutine("EnemySpawnTimer");
+		StartCoroutine("FirstEnemySpawnTimer");
+		StartCoroutine("RandomSpawnTimer");
 	}
-	
-	public void EnemySpawn()
-	{
-		if(randomPos == 0)
-		{
-			randomPos++;
-		}
 
-		if(randomPos == 1)
+	IEnumerator EnemySpawnTimer()
+	{
+		yield return new WaitForSeconds(spawnTimer);
+		EnemySpawn();
+		StartCoroutine("EnemySpawnTimer");
+	}
+
+	IEnumerator FirstEnemySpawnTimer()
+	{
+		yield return new WaitForSeconds(firstEnemyTime);
+
+		if(randomPos == 0)
 		{
 			spawnLocationX = xPositions[0];
 			spawnLocationZ = zPositions[0];
+		}
+		
+		if(randomPos == 1)
+		{
+			spawnLocationX = xPositions[0];
+			spawnLocationZ = zPositions[1];
 		}
 		
 		if(randomPos == 2)
@@ -49,11 +61,91 @@ public class EnemySpawner : MonoBehaviour
 		
 		if(randomPos == 3)
 		{
-			spawnLocationX = xPositions[2];
+			spawnLocationX = xPositions[1];
 			spawnLocationZ = zPositions[1];
 		}
 		
 		if(randomPos == 4)
+		{
+			spawnLocationX = xPositions[2];
+			spawnLocationZ = zPositions[0];
+		}
+		
+		if(randomPos == 5)
+		{
+			spawnLocationX = xPositions[2];
+			spawnLocationZ = zPositions[1];
+		}
+		
+		if(randomPos == 6)
+		{
+			spawnLocationX = xPositions[3];
+			spawnLocationZ = zPositions[0];
+		}
+		
+		if(randomPos == 7)
+		{
+			spawnLocationX = xPositions[3];
+			spawnLocationZ = zPositions[1];
+		}
+		
+		//Debug.Log("Enemy Spawner");
+		Instantiate (thugObj , new Vector3(spawnLocationX , 0 , spawnLocationZ) , Quaternion.identity);
+		iguiScript.thugCount++;
+	}
+
+	IEnumerator RandomSpawnTimer()
+	{
+		yield return new WaitForSeconds(1);
+		randomPos = (int)Random.Range(0 , 8);
+		StartCoroutine("RandomSpawnTimer");
+	}
+	
+	public void EnemySpawn()
+	{
+		if(randomPos == 0)
+		{
+			spawnLocationX = xPositions[0];
+			spawnLocationZ = zPositions[0];
+		}
+		
+		if(randomPos == 1)
+		{
+			spawnLocationX = xPositions[0];
+			spawnLocationZ = zPositions[1];
+		}
+		
+		if(randomPos == 2)
+		{
+			spawnLocationX = xPositions[1];
+			spawnLocationZ = zPositions[0];
+		}
+		
+		if(randomPos == 3)
+		{
+			spawnLocationX = xPositions[1];
+			spawnLocationZ = zPositions[1];
+		}
+
+		if(randomPos == 4)
+		{
+			spawnLocationX = xPositions[2];
+			spawnLocationZ = zPositions[0];
+		}
+
+		if(randomPos == 5)
+		{
+			spawnLocationX = xPositions[2];
+			spawnLocationZ = zPositions[1];
+		}
+
+		if(randomPos == 6)
+		{
+			spawnLocationX = xPositions[3];
+			spawnLocationZ = zPositions[0];
+		}
+
+		if(randomPos == 7)
 		{
 			spawnLocationX = xPositions[3];
 			spawnLocationZ = zPositions[1];
@@ -70,15 +162,8 @@ public class EnemySpawner : MonoBehaviour
 		}
 	}
 
-	IEnumerator EnemySpawnTimer()
-	{
-		yield return new WaitForSeconds(spawnTimer);
-		EnemySpawn();
-		StartCoroutine("EnemySpawnTimer");
-	}
-
 	void Update () 
 	{
-		randomPos = (int)Random.Range(0 , 4);
+
 	}
 }
