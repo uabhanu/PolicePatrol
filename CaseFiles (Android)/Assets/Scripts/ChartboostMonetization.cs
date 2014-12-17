d6004968ed1e40b96d885d651883ff926311767f
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-12-17 14:19:33 +0000

    Chartboost WIP

--- CaseFiles (Android)/Assets/Scripts/ChartboostMonetization.cs
+++ CaseFiles (Android)/Assets/Scripts/ChartboostMonetization.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class ChartboostMonetization : MonoBehaviour 
 {
-	
 	void Start () 
 	{
+		CBExternal.init();
+
 		CBExternal.cacheInterstitial(CBLocation.Default);
 		CBExternal.hasInterstitial(CBLocation.Default);
 		CBExternal.showInterstitial(CBLocation.Default);
@@ -20,6 +21,31 @@ public class ChartboostMonetization : MonoBehaviour
 		CBExternal.hasInPlay(CBLocation.Default);
 		CBExternal.getInPlay(CBLocation.Default);
 	}
+	
+//	public static event Action didCacheInterstitial;
+//	public static event Action didCacheMoreApps;
+//	public static event Action didCacheRewardedVideo;
+//	public static event Action didClickInterstitial;
+//	public static event Action didClickMoreApps;
+//	public static event Action didClickRewardedVideo;
+//	public static event Action didCloseInterstitial;
+//	public static event Action didCloseMoreApps;
+//	public static event Action didCloseRewardedVideo;
+//	public static event Action didDismissInterstitial;
+//	public static event Action didDismissMoreApps;
+//	public static event Action didDismissRewardedVideo;
+//	public static event Action didDisplayInterstitial;
+//	public static event Action didDisplayMoreApps;
+//	public static event Action didDisplayRewardedVideo;
+//	public static event Action<CBLocation> willDisplayVideo;
+//	public static event Action<CBLocation , int> didCompleteRewardedVideo;
+//	public static event Action<CBLocation , CBImpressionError> didFailToLoadInterstitial;
+//	public static event Action<CBLocation , CBImpressionError> didFailToLoadMoreApps;
+//	public static event Action<CBLocation , CBImpressionError> didFailToLoadRewardedVideo;
+//	public static event Action<CBLocation , CBImpressionError> didFailToRecordClick;
+//	public static event Func<CBLocation , bool> shouldDisplayInterstitial;
+//	public static event Func<CBLocation , bool> shouldDisplayMoreApps;
+//	public static event Func<CBLocation, bool> shouldDisplayRewardedVideo;
 
 	public static CBInPlay getInPlay(CBLocation location) 
 	{
@@ -46,8 +72,9 @@ public class ChartboostMonetization : MonoBehaviour
 		return CBExternal.hasRewardedVideo(location);
 	}
 
+
 	void Update () 
 	{
-	
+
 	}
 }
