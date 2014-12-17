6146139062505566c785aef596c193d7bf9db0ca
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-12-17 09:43:36 +0000

    Chartboos WIP

---
+++ CaseFiles (Android)/Assets/Scripts/Monetization.cs
@@ -0,0 +1,16 @@
+﻿using ChartboostSDK;
+using System.Collections;
+using UnityEngine;
+
+public class Monetization : MonoBehaviour 
+{	
+	void Start () 
+	{
+		CBExternal.cacheInterstitial(CBLocation.Default);
+	}
+
+	void Update () 
+	{
+	
+	}
+}
