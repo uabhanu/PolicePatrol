d7892a58e53c3481f89340aaa0f6cbb440ae27fd
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-12-18 11:43:21 +0000

    Level Selection Logic Completed.

--- CaseFiles (Android)/Assets/Scripts/MenuButton.cs
+++ CaseFiles (Android)/Assets/Scripts/MenuButton.cs
@@ -6,12 +6,27 @@ using UnityEngine.UI;
 
 public class MenuButton : MonoBehaviour 
 {
+	public Button button;
+	public ColorBlock cb;
 	public int levelToLoad;
 	public string imageName;
 
 	void Start () 
 	{
+		button = this.gameObject.GetComponent<Button>();
+		cb = button.colors;
 
+		if(imageName.Equals("Spr_Unlock"))
+		{
+			cb.pressedColor = Color.green;
+			button.colors = cb;
+		}
+		
+		if(imageName.Equals("Spr_Lock"))
+		{
+			cb.pressedColor = Color.red;
+			button.colors = cb;
+		}
 	}
 
 	public void ButtonClick(int levelToLoad)
