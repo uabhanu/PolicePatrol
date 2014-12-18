63fbc6f9399b65e979a0a0f8e3acb56b5793233d
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-12-18 09:15:48 +0000

    MenuButton Logic WIP

---
+++ CaseFiles (Android)/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour 
+{	
+	public int levelToLoad;
+
+	void Start () 
+	{
+
+	}
+
+	public void ButtonClick(int levelToLoad)
+	{
+		Application.LoadLevel(levelToLoad);
+	}
+
+	void Update () 
+	{
+	
+	}
+}
