96b8dd79bee6ed8e38060e361bff51996083d3d2
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-12-19 15:31:06 +0000

    Unity Ads set up successfully

---
+++ CaseFiles (Android)/Assets/Scripts/Monetization.cs
@@ -0,0 +1,63 @@
+﻿using ChartboostSDK;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Monetization : MonoBehaviour 
+{
+	public bool unityAdShown;
+
+	void Start () 
+	{
+		Advertisement.Initialize("21734");
+
+		CBExternal.init();
+
+		CBExternal.cacheInterstitial(CBLocation.Default);
+		CBExternal.hasInterstitial(CBLocation.Default);
+		CBExternal.showInterstitial(CBLocation.Default);
+		CBExternal.cacheMoreApps(CBLocation.Default);
+		CBExternal.hasMoreApps(CBLocation.Default);
+		CBExternal.showMoreApps(CBLocation.Default);
+		CBExternal.cacheRewardedVideo(CBLocation.Default);
+		CBExternal.hasRewardedVideo(CBLocation.Default);
+		CBExternal.showRewardedVideo(CBLocation.Default);
+
+		StartCoroutine("UnityAds");
+
+		unityAdShown = false;
+	}
+
+	IEnumerator UnityAds()
+	{
+		yield return new WaitForSeconds(1);
+
+		if(Advertisement.isReady() && !unityAdShown)
+		{
+			Advertisement.Show();
+			unityAdShown = true;
+		}
+
+		StartCoroutine("UnityAds");
+	}
+
+	public static bool hasInterstitial(CBLocation location) 
+	{
+		return CBExternal.hasInterstitial(location);
+	}
+
+	public static bool hasMoreApps(CBLocation location) 
+	{
+		return CBExternal.hasMoreApps(location);
+	}
+
+	public static bool hasRewardedVideo(CBLocation location) 
+	{
+		return CBExternal.hasRewardedVideo(location);
+	}
+	
+	void Update () 
+	{
+
+	}
+}
