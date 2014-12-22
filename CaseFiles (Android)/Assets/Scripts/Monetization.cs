d3514644ac25f519dfaf604affaa071f4d629e1a
Bhanu Upadhyayula <uabhanu@gmail.com>
2014-12-22 11:20:24 +0000

    Brand New Project

--- CaseFiles (Android)/Assets/Scripts/Monetization.cs
+++ CaseFiles (Android)/Assets/Scripts/Monetization.cs
@@ -1,14 +1,14 @@
-﻿using ChartboostSDK;
-using Soomla;
-using Soomla.Highway;
-using Soomla.Levelup;
-using Soomla.Profile;
-using Soomla.Store;
-using Soomla.Store.PolicePatrol;
+﻿//using ChartboostSDK;
+//using Soomla;
+//using Soomla.Highway;
+//using Soomla.Levelup;
+//using Soomla.Profile;
+//using Soomla.Store;
+//using Soomla.Store.PolicePatrol;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Advertisements;
+//using UnityEngine.Advertisements;
 
 public class Monetization : MonoBehaviour 
 {
@@ -17,32 +17,32 @@ public class Monetization : MonoBehaviour
 
 	void Start () 
 	{
-		Advertisement.Initialize("21734");
-
-		CBExternal.init();
-
-		CBExternal.cacheInterstitial(CBLocation.Default);
-		CBExternal.hasInterstitial(CBLocation.Default);
-		CBExternal.showInterstitial(CBLocation.Default);
-		CBExternal.cacheMoreApps(CBLocation.Default);
-		CBExternal.hasMoreApps(CBLocation.Default);
-		CBExternal.showMoreApps(CBLocation.Default);
-		CBExternal.cacheRewardedVideo(CBLocation.Default);
-		CBExternal.hasRewardedVideo(CBLocation.Default);
-		CBExternal.showRewardedVideo(CBLocation.Default);
+//		Advertisement.Initialize("21734");
+//
+//		CBExternal.init();
+//
+//		CBExternal.cacheInterstitial(CBLocation.Default);
+//		CBExternal.hasInterstitial(CBLocation.Default);
+//		CBExternal.showInterstitial(CBLocation.Default);
+//		CBExternal.cacheMoreApps(CBLocation.Default);
+//		CBExternal.hasMoreApps(CBLocation.Default);
+//		CBExternal.showMoreApps(CBLocation.Default);
+//		CBExternal.cacheRewardedVideo(CBLocation.Default);
+//		CBExternal.hasRewardedVideo(CBLocation.Default);
+//		CBExternal.showRewardedVideo(CBLocation.Default);
 
 		levelNo = Application.loadedLevel;
 
 		StartCoroutine("UnityAds");
 
-		StoreEvents.OnSoomlaStoreInitialized += OnSoomlaStoreInitialized;
+		//StoreEvents.OnSoomlaStoreInitialized += OnSoomlaStoreInitialized;
 
-		SoomlaProfile.Initialize();
+		//SoomlaProfile.Initialize();
 		//SoomlaProfileAndroid.Login(Provider.FACEBOOK , "" , null);
 
-		SoomlaStore.Initialize(new InAppPurchases());
+		//SoomlaStore.Initialize(new InAppPurchases());
 
-		StoreEvents.OnMarketPurchase += OnMarketPurchase;
+		//StoreEvents.OnMarketPurchase += OnMarketPurchase;
 
 		unityAdShown = false;
 	}
@@ -51,43 +51,43 @@ public class Monetization : MonoBehaviour
 	{
 		yield return new WaitForSeconds(1);
 
-		if(Advertisement.isReady() && levelNo == 0 && !unityAdShown)
-		{
-			Advertisement.Show();
-			unityAdShown = true;
-		}
+//		if(Advertisement.isReady() && levelNo == 0 && !unityAdShown)
+//		{
+//			Advertisement.Show();
+//			unityAdShown = true;
+//		}
 
 		StartCoroutine("UnityAds");
 	}
 
-	public static bool hasInterstitial(CBLocation location) 
-	{
-		return CBExternal.hasInterstitial(location);
-	}
+//	public static bool hasInterstitial(CBLocation location) 
+//	{
+//		return CBExternal.hasInterstitial(location);
+//	}
+//
+//	public static bool hasMoreApps(CBLocation location) 
+//	{
+//		return CBExternal.hasMoreApps(location);
+//	}
+//
+//	public static bool hasRewardedVideo(CBLocation location) 
+//	{
+//		return CBExternal.hasRewardedVideo(location);
+//	}
 
-	public static bool hasMoreApps(CBLocation location) 
-	{
-		return CBExternal.hasMoreApps(location);
-	}
-
-	public static bool hasRewardedVideo(CBLocation location) 
-	{
-		return CBExternal.hasRewardedVideo(location);
-	}
-
-	public void OnMarketPurchase(PurchasableVirtualItem pvi , string payload , Dictionary<string , string> extra) 
-	{
-		// pvi is the PurchasableVirtualItem that was just purchased
-		// payload is a text that you can give when you initiate the purchase operation and you want to receive back upon completion
-		// extra will contain platform specific information about the market purchase.
-		//      Android: The "extra" dictionary will contain "orderId" and "purchaseToken".
-		//      iOS: The "extra" dictionary will contain "receipt" and "token".
-		
-		//if(persistentScript != null)
-		//{
-			//persistentScript.noAds = true;
-		//}
-	}
+//	public void OnMarketPurchase(PurchasableVirtualItem pvi , string payload , Dictionary<string , string> extra) 
+//	{
+//		// pvi is the PurchasableVirtualItem that was just purchased
+//		// payload is a text that you can give when you initiate the purchase operation and you want to receive back upon completion
+//		// extra will contain platform specific information about the market purchase.
+//		//      Android: The "extra" dictionary will contain "orderId" and "purchaseToken".
+//		//      iOS: The "extra" dictionary will contain "receipt" and "token".
+//		
+//		//if(persistentScript != null)
+//		//{
+//			//persistentScript.noAds = true;
+//		//}
+//	}
 	
 	public void OnSoomlaStoreInitialized()
 	{
