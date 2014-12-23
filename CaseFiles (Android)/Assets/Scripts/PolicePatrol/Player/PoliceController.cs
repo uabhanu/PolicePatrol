6fbb3803b7e5f8e8016206831005779fdf0d2111
Srinath Upadhyayula <srinathupadhyayula@gmail.com>
2014-12-22 19:41:19 -0600

    PoliceController.cs created

---
+++ CaseFiles (Android)/Assets/Scripts/PolicePatrol/Player/PoliceController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoliceController : MonoBehaviour 
+{
+    public float m_moveSpeed;
+    public float m_jumpHeight;
+    
+    public bool m_isFacingRight = true;
+    public bool m_shouldFlip = false;
+    public bool m_isGrounded = false;
+
+    //---------------------------------------------------------------------------------------------------
+	// Use this for initialization
+	void Start () 
+    {
+	   
+	}
+	//---------------------------------------------------------------------------------------------------
+
+    //---------------------------------------------------------------------------------------------------
+	// Update is called once per frame
+	void Update () 
+    {
+        CheckIfGrounded();
+	}
+
+    private void CheckIfGrounded()
+    {
+        
+    }
+
+    //---------------------------------------------------------------------------------------------------
+}
