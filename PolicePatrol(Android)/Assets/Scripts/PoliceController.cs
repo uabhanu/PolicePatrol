b19ae96be489ad5d16f63b328817ec916dc9fbec
Bhanu <uabhanu@gmail.com>
2017-07-09 17:34:15 +0100

    Constable Behaviour WIP Major Clean up WIP

---
+++ PolicePatrol(Android)/Assets/Scripts/PoliceController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PoliceController : MonoBehaviour 
+{
+	#region PlayerState
+	public enum PlayerState
+	{
+		ATTACKING,
+		BLEND,
+		CROUCH,
+		CrouchMoving,
+		DEAD,
+		DYING,
+		IDLE,
+		RUNNING,
+		SLAP,
+		WALKING,
+	};
+	
+	public PlayerState m_currentState;
+	public PlayerState m_previousState;
+    #endregion
+
+    #region Variables Declaration
+        [SerializeField] float m_walkSpeed , m_runSpeed;
+    #endregion
+
+    #region GetState()
+	private PlayerState GetState()
+	{
+		return m_currentState;
+	}
+	#endregion
+
+    #region SetState()
+	public void SetState(PlayerState newState)
+	{
+		if (m_currentState == newState)
+		{
+			return;
+		}
+		
+		m_previousState = m_currentState;
+		m_currentState = newState;
+	}
+	#endregion
+}
---
+++ PolicePatrol(Android)/Assets/Scripts/SriTouchInputListener.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SriTouchInputListener : MonoBehaviour
+{	
+    void Start()
+    {
+
+    }
+	
+    void Update()
+    {
+
+    }
+
+    public void TouchInputListener(TouchInfo touchInfo)
+    {
+        TouchInfo newInfo = touchInfo;
+		SendMessage("TouchInput" , newInfo);
+    }
+}
