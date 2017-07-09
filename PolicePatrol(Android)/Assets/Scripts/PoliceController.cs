2de8e4c0ec6be2a2702bda1370e2ec7eeba38934
Bhanu <uabhanu@gmail.com>
2017-07-09 21:19:24 +0100

    Police Behaviour WIP Major Clean up WIP

--- PolicePatrol(Android)/Assets/Scripts/PoliceController.cs
+++ PolicePatrol(Android)/Assets/Scripts/PoliceController.cs
@@ -3,37 +3,32 @@ using UnityEngine;
 
 public class PoliceController : MonoBehaviour 
 {
-	#region PlayerState
 	public enum PlayerState
 	{
-		ATTACKING,
+		ATTACK,
 		BLEND,
 		CROUCH,
-		CrouchMoving,
+		CROUCHMOVE,
 		DEAD,
 		DYING,
 		IDLE,
-		RUNNING,
+		RUN,
 		SLAP,
-		WALKING,
+		WALK,
 	};
 	
 	public PlayerState m_currentState;
 	public PlayerState m_previousState;
-    #endregion
 
-    #region Variables Declaration
-        [SerializeField] float m_walkSpeed , m_runSpeed;
-    #endregion
+    bool m_isFacingRight , m_isMovingRight;
+    [SerializeField] float m_walkSpeed , m_runSpeed;
+    [SerializeField] Rigidbody2D m_policeBody2D;
 
-    #region GetState()
 	private PlayerState GetState()
 	{
 		return m_currentState;
 	}
-	#endregion
 
-    #region SetState()
 	public void SetState(PlayerState newState)
 	{
 		if (m_currentState == newState)
@@ -44,5 +39,165 @@ public class PoliceController : MonoBehaviour
 		m_previousState = m_currentState;
 		m_currentState = newState;
 	}
-	#endregion
+
+    private void Start()
+    {
+        m_isFacingRight = true;
+        m_isMovingRight = true;
+    }
+
+    private void Update()
+    {
+        if(Time.timeScale == 0)
+        {
+            return;
+        }
+
+       if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SetState(PlayerState.WALK);
+
+        }
+
+       if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            SetState(PlayerState.WALK);
+        }
+
+       if(Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            SetState(PlayerState.IDLE);
+
+        }
+
+       if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            SetState(PlayerState.IDLE);
+        }
+
+       UpdateStateMachine();
+    }
+
+    void Attack()
+    {
+
+    }
+
+    void Blend()
+    {
+
+    }
+
+    void Crouch()
+    {
+
+    }
+
+    void CrouchWalk()
+    {
+
+    }
+
+    void Death()
+    {
+
+    }
+
+    void Dying()
+    {
+
+    }
+
+    void FlipPlayer()
+	{
+		m_isFacingRight = !m_isFacingRight;
+		
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+    void Idle()
+    {
+        m_policeBody2D.velocity = new Vector2(0f , 0f);
+    }
+
+    void Run()
+    {
+
+    }
+
+    void ThighSlap()
+    {
+
+    }
+
+    void Walk()
+    {
+        if(m_isMovingRight)
+		{
+			if(!m_isFacingRight)
+			{
+				FlipPlayer();
+			}
+			
+			m_policeBody2D.velocity = new Vector2(m_walkSpeed , m_policeBody2D.velocity.y);
+		}
+		
+		else if(!m_isMovingRight)
+		{
+			if(m_isFacingRight)
+			{
+				FlipPlayer();
+			}
+			
+			m_policeBody2D.velocity = new Vector2(-m_walkSpeed , m_policeBody2D.velocity.y);
+		}
+    }
+
+    private void UpdateStateMachine()
+	{
+		switch (m_currentState)
+		{
+			case PlayerState.ATTACK:
+				Attack();
+			break;
+
+			case PlayerState.BLEND:
+				Blend();
+			break;
+			
+			case PlayerState.CROUCH:
+				Crouch();
+			break;
+			
+			case PlayerState.CROUCHMOVE:
+				CrouchWalk();
+			break;
+			
+			case PlayerState.DEAD: 
+				Death();
+			break;
+			
+			case PlayerState.DYING: 
+				Dying();
+			break;
+			
+			case PlayerState.IDLE: 
+				Idle();
+			break;
+			
+			case PlayerState.RUN:
+				Run();
+			break;
+			
+			case PlayerState.SLAP: 
+				ThighSlap();
+			break;
+			
+			case PlayerState.WALK: 
+				Walk();
+			break;
+		}
+	}
 }
